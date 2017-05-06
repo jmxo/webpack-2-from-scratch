c48b83dbfb258e95bfbe27e64f721697738bdd8b
mohamed-ismat <mnb1236@live.com>
2017-05-06 18:32:26 +0300

    loading images

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import cat from '../assets/images/cat.jpg';
+import dog from '../assets/images/dog.png';
+import dogs from '../assets/images/dogs.gif';
 
-const Hello = () =>
-  <div>
-    <h1>Hello from React</h1>
-  </div>
 
-export default Hello;
+const hello = () =>
+    <div>
+      <div className="container">
+        <div className="image-wrapper">
+          <img src={cat} className="image-wrapper__image" alt=""/>
+
+        </div>
+        <div className="image-wrapper">
+          <img src={dog} className="image-wrapper__image" alt=""/>
+
+        </div>
+        <div className="image-wrapper">
+          <img src={dogs} className="image-wrapper__image" alt=""/>
+
+        </div>
+      </div>
+    </div>
+
+
+
+export default hello;
