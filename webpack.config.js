b64bf9efc6fef6e25b221054fdcb8b2c1110f975
mohamed-ismat <mnb1236@live.com>
2017-05-06 16:37:36 +0300

    extract transpiled css

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 let config = {
-  entry: './src/index.js',
+
+  entry: './src/index.js', // entry file
   output: {
-    path: path.resolve(__dirname,'./public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, './public'), // ouput path
+    filename: 'bundle.js' // output filename
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/, // files ending with .js
         exclude: /node_modules/, // exclude the node_modules directory
         loader: "babel-loader" // use this (babel-core) loader
       },
       {
         test: /\.scss$/, // files ending with .scss
-        loader: ['style-loader', 'css-loader', 'sass-loader'] // use these loaders
+        use: ExtractTextWebpackPlugin.extract({ // call our plugin with extract method
+          use: ['css-loader', 'sass-loader'], // use these loaders
+          fallback: 'style-loader' // fallback for any CSS not extracted
+        }) // end extract
       }
-    ]
-  }
+    ] // end rules
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin constructor and name our css file
+  ]
 }
 
 module.exports = config;
